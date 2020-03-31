0c662a0e149f9fcaa757d0588f62ea175815d1ab
EduBic <bic.studio@yahoo.it>
2020-03-31 23:35:09 +0200

    Add BFS to tree
    Co-authored-by: Lorissagg93 <lorissagg93@gmail.com>

--- Day6/main.cpp
+++ Day6/main.cpp
@@ -3,60 +3,78 @@
 
 using namespace std;
 
+template<class T> 
 class Node
 {
 public:
 
-    Node(string value): value(value), child(nullptr) {}
+    Node(T value): value(value), child(nullptr) {}
 
-    string getValue() const {
+    T getValue() const {
         return this->value;
     }
 
-    void setChild(Node* child) {
+    void setChild(Node<T>* child) {
         this->child = child;
     }
 
-    Node* getChild() {
+    Node<T>* getChild() {
         return this->child;
     }
 
 private:
-    string value;
-    Node* child;
+    T value;
+    Node<T>* child;
 };
 
 template<class T> 
 class List
 {
 public:
-    List(): first(nullptr) {}
+    List(): first(nullptr), size(0) {}
 
-    void push_back(T* newItem) {
+    void push_back(T newItem) {
         // assert child != nullptr
         if (first == nullptr) 
         {
-            first = newItem;
+            first = new Node<T>(newItem);
         }
         else 
         {
-            for (T* iter = first; iter != nullptr; iter = iter->getChild()) 
+            for (Node<T>* iter = first; iter != nullptr; iter = iter->getChild()) 
             {
                 if (iter->getChild() == nullptr) {
                     // end of list
-                    iter->setChild(newItem);
+                    iter->setChild(new Node<T>(newItem));
                     break;
                 }
             }
         }
+        size++;
     }
 
-    T* next() {
-        return this->first;
+    T pop_front() {
+        Node<T>* oldFirst = first;
+        first = oldFirst->getChild();
+        T frontValue = oldFirst->getValue();
+        delete oldFirst;
+
+        size--;
+        return frontValue;
+    }
+
+    Node<T>* next() {
+        return first;
+    }
+
+    bool is_empty() const
+    {
+        return first == nullptr;
     }
 
 private:
-    T* first;
+    Node<T>* first;
+    int size;
 };
 
 
@@ -64,7 +82,7 @@ class TreeNode
 {
 public:
 
-    TreeNode(string value): value(value), children(new List<TreeNode>()) {}
+    TreeNode(string value): value(value), children(new List<TreeNode*>()) {}
 
     string getValue() const {
         return this->value;
@@ -74,53 +92,66 @@ public:
         children->push_back(newChild);
     }
 
-    List<TreeNode>* get_children() {
+    List<TreeNode*>* get_children() {
         return this->children;
+    }
+
+    bool has_children() {
+        return !children->is_empty();
     }
 
 private:
     string value;
-    List<TreeNode>* children;
+    List<TreeNode*>* children;
 };
 
 
 int main()
 {
-    TreeNode* sun = new TreeNode("Sun");
+    TreeNode* sun = new TreeNode("1-Sun");
 
-    TreeNode* mercury = new TreeNode("Mercury");
+    TreeNode* mercury = new TreeNode("2-Mercury");
 
-    TreeNode* venus = new TreeNode("Venus");
+    TreeNode* venus = new TreeNode("3-Venus");
 
-    TreeNode* earth = new TreeNode("Earth");
-    TreeNode* moon = new TreeNode("Moon");
+    TreeNode* earth = new TreeNode("4-Earth");
+    TreeNode* moon = new TreeNode("6-Moon");
     earth->push_child(moon);
 
-    TreeNode* mars = new TreeNode("Mars");
-    TreeNode* deimos = new TreeNode("Deimos");
-    TreeNode* phobos = new TreeNode("Phobos");
+    TreeNode* mars = new TreeNode("5-Mars");
+    TreeNode* deimos = new TreeNode("7-Deimos");
+    TreeNode* phobos = new TreeNode("8-Phobos");
     mars->push_child(deimos);
     mars->push_child(phobos);
-
+    TreeNode* I1 = new TreeNode("9-I1");
+    moon->push_child(I1);
+    TreeNode* I2 = new TreeNode("10-I2");
+    phobos->push_child(I2);
+    
     sun->push_child(mercury);
     sun->push_child(venus);
     sun->push_child(earth);
     sun->push_child(mars);
 
-    
-    cout << sun->getValue() << endl;
-    List<TreeNode>* sunChildren = sun->get_children();
-    TreeNode* iter = sunChildren->next();
+    // cout all nodes
+    List<TreeNode*>* supportList = new List<TreeNode*>();
+    supportList->push_back(sun); // first node
 
-    // support list <- children of Sun
-    // iter = supportList->next()
-    while (iter != nullptr)
+    Node<TreeNode*>* iter = supportList->next();
+    while (!supportList->is_empty())
     {
+        TreeNode* currentTreeNode = supportList->pop_front();
         // use the value of node
-        cout << sun->getValue() << endl;
+        cout << currentTreeNode->getValue() << endl;
 
-        // if iter->children is not empty()
-        // add these children to the support list
-        // iter = iter->next();
+        if (currentTreeNode->has_children()) {
+            List<TreeNode*>* children = currentTreeNode->get_children();
+            for (Node<TreeNode*>* childIter = children->next(); 
+                childIter != nullptr; 
+                childIter = childIter->getChild())
+            {
+                supportList->push_back(childIter->getValue());
+            }
+        }
     }
 }
